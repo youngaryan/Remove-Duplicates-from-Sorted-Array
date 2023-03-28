4e62541635f69082e66606d2f7c310e2846cfe38
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-03-28 20:28:00 +0100

    got bored of iy

---
+++ Solution.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+import java.util.TreeSet;
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+        }
+        return 0;
+    }
+}
---
+++ main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        Solution solution= new Solution();
+        int[] l = new int[]{1, 1, 2};
+        System.out.println(solution.removeDuplicates(l));
+    }
+
+}
